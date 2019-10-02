df34e7d469ebada64aba4ca2c5092ea65c4a9c6e
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-02 11:59:41 +0200

    create new LevelInfo component

--- src/components/Description.js
+++ src/components/Description.js
@@ -18,26 +18,7 @@ const Description = (props) => {
                 .filter((milstone) => milstone.level.includes(attributeObject.currentLevel))
                 .map((milestone) => {
                   return (
-                    <>
-                      <div className="levelInfo__number">{attributeObject.currentLevel}</div>
-                      <p className="levelInfo__summary">{milestone.summary}</p>
-                      <h4 className="levelInfo__behaviors">Examples behaviors</h4>
-                      <ul className="levelInfo__behavior-list">
-                        {milestone.signals.map((signal, index) => {
-                          return (
-                            <li className="levelInfo__behavior-item" key={index}>{signal}</li>
-                          );
-                        })}
-                      </ul>
-                      <h4 className="levelInfo__tasks">Examples Tasks</h4>
-                      <ul className="levelInfo__task-list">
-                        {milestone.examples.map((example, index) => {
-                          return (
-                            <li className="levelInfo__task-item" key={index}>{example}</li>
-                          );
-                        })}
-                      </ul>
-                    </>
+                    <LevelInfo milestone={milestone} attributeObject={attributeObject} />
                   );
                 })}
             </div>
