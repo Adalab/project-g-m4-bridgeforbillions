a129ba55b1c5877d07f3751461cfff32c0efe5cd
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-10-02 18:31:11 +0200

    subtitle style

--- src/components/LevelInfo.js
+++ src/components/LevelInfo.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './../styles/levelInfo.css';
+import { Typography } from 'antd';
 
 
 const LevelInfo = (props) => {
   const { milestone, attributeObject } = props;
+  const { Title } = Typography;
 
   return (
     <div>
       <div className={`levelInfo__number--${attributeObject.category}`}>{attributeObject.currentLevel}</div>
       <p className="levelInfo__summary">{milestone.summary}</p>
-      <h4 className="levelInfo__behaviors">Examples behaviors</h4>
+      <Title level={4} className="levelInfo__behaviors title__h4">Examples behaviors</Title>
       <ul className="levelInfo__behavior-list">
         {milestone.signals.map((signal, index) => {
           return (
@@ -18,7 +20,7 @@ const LevelInfo = (props) => {
           );
         })}
       </ul>
-      <h4 className="levelInfo__tasks">Examples Tasks</h4>
+      <Title level={4} className="levelInfo__taskst title__h4">Examples Tasks</Title>
       <ul className="levelInfo__task-list">
         {milestone.examples.map((example, index) => {
           return (
