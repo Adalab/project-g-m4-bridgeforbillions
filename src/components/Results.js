d062ea6c95e7ce406bbd577d75ca84b8fb78b936
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-10-03 14:11:34 +0200

    revised general

--- src/components/Results.js
+++ src/components/Results.js
@@ -8,27 +8,26 @@ const Results = () => {
   return (
     <div className="results__container">
       <Title className="results__title" level={1}>Results</Title>
-	  <Row className="points__section">
-		  <Col xs={{ span:24 }} sm={{ span:7, offset:1 }} md={{ span:7, offset:1 }}>
-        <div className="points__container">
-          <div className="points__number">###</div>
-          <p className="points__subtitle">Total points</p>
-        </div>
-		  </Col>
-		  <Col xs={{ span:24 }} sm={{ span:7, offset:1 }} md={{ span:7, offset:1 }}>
-        <div className="points__container">
-          <div className="points__number">###</div>
-          <p className="points__subtitle">Total points</p>
-        </div>
-		  </Col>
-		  <Col xs={{ span:24 }} sm={{ span:7, offset:1 }} md={{ span:7, offset:1 }}>
+      <Row className="points__section">
+        <Col xs={{ span: 24 }} sm={{ span: 7, offset: 1 }} md={{ span: 7, offset: 1 }}>
           <div className="points__container">
-          <div className="points__number">###</div>
-          <p className="points__subtitle">Points to next level</p>
-        </div>
-		  </Col>
-		</Row>
-	  
+            <div className="points__number">###</div>
+            <p className="points__subtitle">Total points</p>
+          </div>
+        </Col>
+        <Col xs={{ span: 24 }} sm={{ span: 7, offset: 1 }} md={{ span: 7, offset: 1 }}>
+          <div className="points__container">
+            <div className="points__number">###</div>
+            <p className="points__subtitle">Total points</p>
+          </div>
+        </Col>
+        <Col xs={{ span: 24 }} sm={{ span: 7, offset: 1 }} md={{ span: 7, offset: 1 }}>
+          <div className="points__container">
+            <div className="points__number">###</div>
+            <p className="points__subtitle">Points to next level</p>
+          </div>
+        </Col>
+      </Row>
       <div className="percentage__section">
         <Progress className="percentage__circle" type="circle" percent={75} strokeColor="#5C5CEA" strokeWidth={10} />
         <Progress className="percentage__circle" type="circle" percent={70} strokeColor="#A8BC3B" strokeWidth={10} />
